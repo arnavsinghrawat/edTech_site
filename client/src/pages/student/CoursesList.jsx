f330d3a34a0f578bbafebc12f54c9ca5d9d7378e
arnavsinghrawat <arnavrawat76@gmail.com>
2025-03-16 19:33:43 +0530

    worked on course detail page

--- client/src/pages/student/CoursesList.jsx
+++ client/src/pages/student/CoursesList.jsx
@@ -49,7 +49,6 @@ const CoursesList = () => {
         </div>}
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 my-16 gap-3 px-2 md:p-0'>
         {filteredCourse.map((course,index)=><CourseCart key={index} course={course}/>)}
-
       </div>
     </div>
 
