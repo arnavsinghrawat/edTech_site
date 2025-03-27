c61e9255bb04b8376a076f4d87541bd47700ea9b
arnavsinghrawat <arnavrawat76@gmail.com>
2025-03-27 07:52:08 +0530

    working dashboard Mycourses, and students enrolled

--- client/src/pages/educator/AddCourse.jsx
+++ client/src/pages/educator/AddCourse.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import uniqid from 'uniqid';
+import Quill from 'quill';
 
 const AddCourse = () => {
+
+  const quillRef = useRef(null);
+  const editorRef = useRef(null);
+
+  const [courseTitle, setCourseTitle] = useState('');
+  const [coursePrice, setCoursePrice] = useState(0);
+  const [discount, setDiscount] = useState(0);
+  const [image, setImage] = useState(null);
+  const [chapters, setChapters] = useState([]);
+  const [showPopup, setShowPopup] = useState(false);
+
   return (
     <div>
       <h1>Educator Add courses</h1>
