3f6b2eb3617db81bf42c593880defe2955416f5c
arnavsinghrawat <arnavrawat76@gmail.com>
2025-03-30 18:41:36 +0530

    worked on webhooks for user data

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from './configs/mongodb.js'
 
 //intialize express
 const app = express()
+
+//connect to database
+await connectDB();
+
+//middleware
 app.use(cors());
 
 //routes
