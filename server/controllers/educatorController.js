664a43f27d25dc141abaa95fa17c154c6eae2af0
arnavsinghrawat <arnavrawat76@gmail.com>
2025-04-05 12:13:46 +0530

    stripe is working

--- server/controllers/educatorController.js
+++ server/controllers/educatorController.js
@@ -1,6 +1,7 @@
 import {clerkClient} from '@clerk/express'
 import Course from '../models/Course.js';
 import {v2 as cloudinary} from 'cloudinary';
+import { Purchase } from '../models/Purchase.js';
 
 //update role to educator
 export const updateRoleToEducator = async (req, res)=>{
@@ -45,6 +46,91 @@ export const addCourse = async (req,res) => {
     }catch(error){
 
         res.json({success: false, message: error.message});
-        
+
+    }
+}
+
+//get educator courses
+export const getEducatorCourses = async (req, res) => {
+    try
+    {
+        const educator = req.auth.userId
+        const courses = await Course.find(({educator}));
+        res.json({success: true, courses})
+    }
+    catch(error)
+    {
+        res.json({success: false, message: error.message})
+    }
+
+}
+
+// get educator dashboard data(total Earning, enrolled Students, No of courses)
+
+export const educatorDashboardData = async(req, res) => {
+    try{
+        const educator = req.auth.userId;
+        const courses = await Course.find({educator});
+        const totalCourses = courses.length;
+
+        const courseIds = courses.map(course => course._id);
+
+        const purchases = await Purchase.find({
+            courseId: {$in: courseIds},
+            status: 'completed'
+        });
+
+        const totalEarnings = purchases.reduce((sum,purchase) => sum + purchase.amount, 0);
+
+        const enrolledStudentsData = [];
+        for(const course of courses)
+        {
+            const students = await User.find({
+                _id: {$in: course.enrolledStudents}
+            }, 'name imageUrl');
+
+            students.forEach(student => {
+                enrolledStudentsData.push({
+                    courseTitle: course.courseTitle,
+                    student
+                })
+            });
+        }
+        res.json({success: true, dashboardData: {
+            totalEarnings, enrolledStudentsData, totalCourses
+        }})
+
+    } catch(error)
+    {
+        res.json({success: false, message: error.message});
+    }
+}
+
+
+//get enrolled studetns data with the purchase data
+
+export const getEnrolledStudentsData = async (req, res) => {
+    try{
+        const educator = req.auth.userId;
+        const courses = await Course.find({educator});
+
+        const courseIds = courses.map(course => course._id);
+
+        const purchases = await Purchase.find({
+            courseId: {$in: courseIds },
+            status: 'completed'
+        }).populate('userId', 'name imageUrl').populate('courseId','courseTitle')
+
+        const enrolledStudents = purchases.map(purchase => ({
+            student: purchase.userId,
+            courseTitle: purchase.courseId.courseTitle,
+            purchaseData: purchase.createdAt
+        }))
+
+        res.json({success: true, enrolledStudents})
+
+    }catch(error)
+    {
+        res.json({success: false, message: error.message})
     }
 }
